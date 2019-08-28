d3aaf7360c2e4ea883f745a0f05cd7b42b5b4cea
NoSkillWill <54608745+NoSkillWill@users.noreply.github.com>
2019-08-28 09:49:16 +0800

    Add files via upload

---
+++ scripts/config_xml.js
@@ -0,0 +1,50 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2019-08-27 02:29:12 PM" dc:source="Camtasia,18.0.1,enu" dc:title="tie wednesday" tscDM:firstFrame="tie_wednesday_First_Frame.png" tscDM:originId="DB736C10-C21A-459E-9B43-456BBA3039C1" tscDM:project="Untitled Project1">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="42200"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="628" stDim:w="1120"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="D14DA6EF-6A36-4158-95C0-CD9D4FEB7812" tscIQ:authoredEmail="aZEVcZsO6yHjMEcXfmy/tF/3vmEnfEvu8obul0A4W5KGHvEDT1AcJG2ej36oGWwydUhV00/Td4Mw\
+zWs5M47SVQwXszoLUHRRr793Jk1d09/+PZSDCItkkeSgG7aa87s5cxdwOY6o2D6vZxsCS/ZwSoMI\
+o3Z1MtOHMMOF8gx8bAhJwDWENYw5bAHulgzN1/uk23bFCpKr49sBP0VwtY3omQ9Ow2tDIDOSFY2h\
+56YYbDygMKLc/E8MZdt45wPxklb/vxY7ZGsyVIOlkUwJXzz4eb5ubvq8MvgQxlqEBUroQpBvTmd0\
+TjHFeqO1uz0bL2I3Tg0v1UgZUPiq9ylvgqdZKQ==" tscIQ:requireUserId="0" tscIQ:locale="en-US" tscIQ:reportMethod="API" tscIQ:allowSkipQuiz="0" tscIQ:clientId="0E45EC2A-7CCA-4294-84CE-CB0980980952" tscIQ:hideReplay="0" tscIQ:quizHash="a6867867d8f6c5e5cbf33a3e41f3b60b">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:startTime="33800" tscIQ:feedback="1" tscIQ:questionSetName="Quiz 1"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="0"><tscIQ:question>What day of the week is Tie Wednesday on?</tscIQ:question><tscIQ:correctAnswer>1</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>Wednesday</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>Tuesday</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="2"><tscIQ:answer>All of the Above</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tscIQ:QuizParams><rdf:Bag><rdf:li xmpDM:name="txtPrev" xmpDM:value="Previous"/><rdf:li xmpDM:name="txtNext" xmpDM:value="Next"/><rdf:li xmpDM:name="txtAnswerQuestion" xmpDM:value="Take Quiz Now"/><rdf:li xmpDM:name="txtSubmit" xmpDM:value="Submit Answers"/><rdf:li xmpDM:name="txtReview" xmpDM:value="Replay Last Section"/><rdf:li xmpDM:name="txtReviewAnswer" xmpDM:value="View Answers"/><rdf:li xmpDM:name="txtContinue" xmpDM:value="Continue"/></rdf:Bag></tscIQ:QuizParams></rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="tie wednesday.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="tie_wednesday_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="tie_wednesday_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
